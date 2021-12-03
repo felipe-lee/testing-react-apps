9a0e3e9aedee932cc47bc0ef0e1e3793fd7ec284
Felipe Lee <felipe@truss.works>
2021-12-03 12:13:55 -0800

    Swap login form factory with `test-data-bot` builder

--- src/__tests__/exercise/04.extra-3.js
+++ src/__tests__/exercise/04.extra-3.js
@@ -4,18 +4,19 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
+import faker from 'faker'
 import Login from '../../components/login'
 
-const loginFormBuilder = build({
-  fields: {
-    username: fake(f => f.internet.userName()),
-    password: fake(f => f.internet.password()),
+const buildLoginForm = (overrides) => (
+  {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
   }
-})
+)
 
 test('submitting the form calls onSubmit with username and password', () => {
-  const {username, password} = loginFormBuilder()
+  const {username, password} = buildLoginForm()
 
   const handleSubmit = jest.fn()
 
