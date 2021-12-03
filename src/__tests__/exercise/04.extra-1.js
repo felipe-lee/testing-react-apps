7cfd7aac6cd7b5922808ba4e4624279b3e60be86
Felipe Lee <felipe@truss.works>
2021-12-03 11:59:59 -0800

    Replace hard-coded fake data with `faker` data

--- src/__tests__/exercise/04.extra-1.js
+++ src/__tests__/exercise/04.extra-1.js
@@ -4,18 +4,13 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
 import Login from '../../components/login'
 
-const buildLoginForm = () => (
-  {
-    username: faker.internet.userName(),
-    password: faker.internet.password()
-  }
-)
-
 test('submitting the form calls onSubmit with username and password', () => {
-  const {username, password} = buildLoginForm()
+  const expectedData = {
+    username: 'bestcat',
+    password: 'allthecats'
+  }
 
   const handleSubmit = jest.fn()
 
@@ -24,13 +19,13 @@ test('submitting the form calls onSubmit with username and password', () => {
   const usernameField = screen.getByLabelText(/username/i)
   const passwordField = screen.getByLabelText(/password/i)
 
-  userEvent.type(usernameField, username)
-  userEvent.type(passwordField, password)
+  userEvent.type(usernameField, expectedData.username)
+  userEvent.type(passwordField, expectedData.password)
 
   const submitButton = screen.getByRole('button', { name: /submit/i })
 
   userEvent.click(submitButton)
 
-  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledWith(expectedData)
   expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
