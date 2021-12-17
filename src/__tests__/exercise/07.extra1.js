2ae826ab3292f7596cb47b75f79ebfe1e479c569
Felipe Lee <felipe@truss.works>
2021-12-17 13:57:07 -0800

    Pull wrapper out to custom render function

--- src/__tests__/exercise/07.extra1.js
+++ src/__tests__/exercise/07.extra1.js
@@ -6,19 +6,16 @@ import {render, screen} from '@testing-library/react'
 import EasyButton from 'components/easy-button'
 import {ThemeProvider} from 'components/theme'
 
-const renderWithTheme = (ui, {theme = 'light', ...options} = {}) => {
-  const Wrapper = ({children}) => {
-    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  }
-
-  return render(ui, {wrapper: Wrapper, ...options})
-}
 
 test.each([
   {theme: 'light', backgroundColor: 'white', color: 'black'},
   {theme: 'dark', backgroundColor: 'black', color: 'white'},
 ])('renders with the expected styles for each theme', ({theme, backgroundColor, color}) => {
-  renderWithTheme(<EasyButton>Easy</EasyButton>, {theme})
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  }
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
 
   const button = screen.getByRole('button', {name: /easy/i})
 
