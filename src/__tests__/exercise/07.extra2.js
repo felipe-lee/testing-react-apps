d921f676b025ff29e21d81379cfc6a1f26880f06
Felipe Lee <felipe@truss.works>
2021-12-17 14:00:39 -0800

    Swap to use provided test/test-utils

--- src/__tests__/exercise/07.extra2.js
+++ src/__tests__/exercise/07.extra2.js
@@ -2,14 +2,23 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
+import {render, screen} from '@testing-library/react'
 import EasyButton from 'components/easy-button'
-import {render, screen} from 'test/test-utils'
+import {ThemeProvider} from 'components/theme'
+
+const renderWithTheme = (ui, {theme = 'light', ...options} = {}) => {
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  }
+
+  return render(ui, {wrapper: Wrapper, ...options})
+}
 
 test.each([
   {theme: 'light', backgroundColor: 'white', color: 'black'},
   {theme: 'dark', backgroundColor: 'black', color: 'white'},
 ])('renders with the expected styles for each theme', ({theme, backgroundColor, color}) => {
-  render(<EasyButton>Easy</EasyButton>, {theme})
+  renderWithTheme(<EasyButton>Easy</EasyButton>, {theme})
 
   const button = screen.getByRole('button', {name: /easy/i})
 
